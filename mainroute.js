9dbe8cac3412a7711f950d8baa3f0fd18ee60478
Shivam Jain <90458496+Shivam-jainn@users.noreply.github.com>
2023-03-05 22:50:20 +0530

    updated and made landing page router

--- mainroute.js
+++ mainroute.js
@@ -5,6 +5,9 @@ const app = express();
 const port = 3000
 app.listen(port);
 
+const eduSchoolRouter = require('./src/Routes/landingpagerouter.js');
+app.use('/', landingpagerouter);
+
 const eduSchoolRouter = require('./src/Routes/eduSchoolRoute.js');
 app.use('/school', eduSchoolRouter);
 
